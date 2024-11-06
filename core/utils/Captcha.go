a7dc51df0151fd54a1355ea4bb24891d84c0279e
吃掉大象的蚂蚁 <eoe2005@qq.com>
2024-11-06 16:29:58 +0800

    fx

--- core/utils/Captcha.go
+++ core/utils/Captcha.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"crypto/rand"
+	_ "embed"
 	"fmt"
 	"image"
 	"image/color"
--- main.go
+++ main.go
@@ -2,36 +2,10 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
 
-	"github.com/ghf-go/fleetness/core"
-	"github.com/ghf-go/fleetness/core/log"
-	"github.com/ghf-go/fleetness/core/session"
+	"github.com/ghf-go/fleetness/demo"
 )
 
-//go:embed test.yaml
-var _confData []byte
-
 func main() {
-	ge := core.NewGengine(_confData)
-	api := ge.RouterGroup("api", func(c *core.GContent) {
-		log.Debug(c, "das")
-		c.FailJson(404, "接口怒存在")
-
-	}, session.SessionJwt("12312312"),
-		func(c *core.GContent) {
-			log.Debug(c, "api start")
-			c.Next()
-			log.Debug(c, "api end")
-		})
-	api.Get("login", func(c *core.GContent) {
-		fmt.Printf("处理的信息login\n")
-		c.SetUserID("1234324")
-		c.SuccessJson("123")
-	})
-	api.Get("zz", func(c *core.GContent) {
-		fmt.Printf("zz\n")
-		c.SuccessJson("123")
-	})
-	ge.Run()
+	demo.AppRun()
 }
