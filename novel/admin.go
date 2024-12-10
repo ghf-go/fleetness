4b8e137d2c55913a200081128619d17050b5336e
吃掉大象的蚂蚁 <eoe2005@qq.com>
2024-12-11 01:10:04 +0800

    小说发布相关的和管理后台相关的代码

--- novel/admin.go
+++ novel/admin.go
@@ -1,21 +1,136 @@
 package novel
 
-import "github.com/ghf-go/fleetness/core"
+import (
+	"github.com/ghf-go/fleetness/core"
+	"github.com/ghf-go/fleetness/novel/model"
+)
 
 // 待审核小说
-func adminNovelWaitAuditAction(c *core.GContent) {}
+func adminNovelWaitAuditAction(c *core.GContent) {
+	p := &core.PageParam{}
+	if e := c.BindJson(p); e != nil {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	db := getDB(c)
+	total := int64(0)
+	list := []model.NovelInfo{}
+	db.Model(&model.NovelInfo{}).Where("is_audit=0").Count(&total)
+	db.Where("is_audit=0").Order("id ASC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&list)
+	c.SuccessJson(map[string]any{
+		"total": total,
+		"list":  list,
+	})
+}
 
 // 审核小数
-func adminNovelAuditAction(c *core.GContent) {}
+func adminNovelAuditAction(c *core.GContent) {
+	p := &core.PageParam{}
+	if e := c.BindJson(p); e != nil || p.ID == 0 {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	if getDB(c).Model(&model.NovelInfo{}).Where(p.ID).Update("is_audit", 1).Error != nil {
+		c.FailJson(403, "审核失败")
+		return
+	}
+	c.SuccessJson("success")
+}
 
 // 待审核章节
-func adminSectionWaitAuditAction(c *core.GContent) {}
+func adminSectionWaitAuditAction(c *core.GContent) {
+	p := &core.PageParam{}
+	if e := c.BindJson(p); e != nil {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	db := getDB(c)
+	total := int64(0)
+	list := []model.NovelSection{}
+	clist := []model.NovelSectionContent{}
+	cMap := map[uint64]string{}
+	db.Model(&model.NovelSection{}).Where("is_audit=0").Count(&total)
+	db.Where("is_audit=0").Order("id ASC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&list)
+	cids := []uint64{}
+	for _, item := range list {
+		cids = append(cids, item.ID)
+	}
+	if len(cids) > 0 {
+		db.Find(&clist, cids)
+		for _, item := range clist {
+			cMap[item.SectionID] = item.Content
+		}
+	}
+	retList := []map[string]any{}
+	for _, item := range list {
+		if sc, isOk := cMap[item.ID]; isOk {
+			retList = append(retList, map[string]any{
+				"content": sc,
+				"name":    item.Name,
+				"id":      item.ID,
+			})
+		} else {
+			retList = append(retList, map[string]any{
+				"content": "",
+				"name":    item.Name,
+				"id":      item.ID,
+			})
+		}
+
+	}
+	c.SuccessJson(map[string]any{
+		"total": total,
+		"list":  retList,
+	})
+}
 
 // 审核章节
-func adminSectionAduitAction(c *core.GContent) {}
+func adminSectionAduitAction(c *core.GContent) {
+	p := &core.PageParam{}
+	if e := c.BindJson(p); e != nil || p.ID == 0 {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	if getDB(c).Model(&model.NovelSection{}).Where(p.ID).Update("is_audit", 1).Error != nil {
+		c.FailJson(403, "审核失败")
+		return
+	}
+
+	c.SuccessJson("success")
+}
 
 // 提现列表
-func adminTxListAction(c *core.GContent) {}
+func adminTxListAction(c *core.GContent) {
+	p := &core.PageParam{}
+	if e := c.BindJson(p); e != nil {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	list := []model.NovelTxLog{}
+
+	db := getDB(c)
+	total := int64(0)
+	switch p.TabName {
+	case "wait":
+		db.Model(&model.NovelTxLog{}).Where("status=0").Count(&total)
+		db.Where("status=0").Order("id DESC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&list)
+	case "audit":
+		db.Model(&model.NovelTxLog{}).Where("status=10").Count(&total)
+		db.Where("status=10").Order("id DESC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&list)
+	case "done":
+		db.Model(&model.NovelTxLog{}).Where("status=20").Count(&total)
+		db.Where("status=20").Order("id DESC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&list)
+	default:
+		db.Model(&model.NovelTxLog{}).Count(&total)
+		db.Order("id DESC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&list)
+	}
+
+	c.SuccessJson(map[string]any{
+		"total": total,
+		"list":  list,
+	})
+
+}
 
 // 提现付款
 func adminTxPayAction(c *core.GContent) {}
--- novel/api_author.go
+++ novel/api_author.go
@@ -1,8 +1,12 @@
 package novel
 
 import (
+	"errors"
+	"time"
+
 	"github.com/ghf-go/fleetness/core"
 	"github.com/ghf-go/fleetness/novel/model"
+	"gorm.io/gorm"
 )
 
 // 创建小说
@@ -67,16 +71,173 @@ func apiAuthorNovelListAction(c *core.GContent) {
 }
 
 // 详情
-func apiAuthorNovelInfoAction(c *core.GContent) {}
+func apiAuthorNovelInfoAction(c *core.GContent) {
+	p := &core.ApiParam{}
+	if e := c.BindJson(p); e != nil || p.ID == 0 {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	uid := c.GetUserID()
+	db := getDB(c)
+	info := &model.NovelInfo{}
+	db.First(info, p.ID)
+	if info.ID == 0 || info.UserID != uid {
+		c.FailJson(403, "内容不存在")
+		return
+	}
+	slist := []model.NovelSection{}
+	db.Order("id DESC").Find(&slist, "novel_id=?", p.ID)
+
+	c.SuccessJson(map[string]any{
+		"info":      info,
+		"sections:": slist,
+	})
+
+}
+
+type apiAuthorSectionSaveActionParam struct {
+	NovelID uint64 `json:"novel_id"`
+	Name    string `json:"name"`
+	Content string `json:"name"`
+	core.ApiParam
+}
 
 // 保存发布章节
-func apiAuthorSectionSaveAction(c *core.GContent) {}
+func apiAuthorSectionSaveAction(c *core.GContent) {
+	p := &apiAuthorSectionSaveActionParam{}
+	if e := c.BindJson(p); e != nil || p.Name == "" || p.NovelID == 0 {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	db := getDB(c)
+	uid := c.GetUserID()
+	ip := c.GetIP()
+	info := &model.NovelInfo{}
+	db.First(info, p.ID)
+	if info.ID == 0 || info.UserID != uid {
+		c.FailJson(403, "内容不存在")
+		return
+	}
+
+	lastRow := &model.NovelSection{}
+
+	db.Where("novel_id=?", p.NovelID).Order("ID DESC").First(lastRow)
+	row := &model.NovelSection{
+		CreateIP: ip,
+		UpdateIP: ip,
+		NovelID:  p.NovelID,
+		Name:     p.Name,
+		ID:       p.ID,
+	}
+	if row.ID > 0 {
+		row.SectionIndex = lastRow.SectionIndex + 1
+	}
+
+	if p.Content == "" { //发布大章节
+		if lastRow.ID > 0 && lastRow.Words == 0 {
+			c.FailJson(403, "不能联系发布大章节")
+			return
+		}
+
+		if db.Save(row).Error == nil {
+			c.SuccessJson("success")
+			return
+		}
+		c.FailJson(403, "发布失败")
+		return
+	} else { //发布章节
+		row.Words = uint(len([]rune(p.Content)))
+		c.Tx(db, func(tx *gorm.DB) (error, any) {
+			if tx.Save(row).Error != nil || tx.Save(&model.NovelSectionContent{
+				CreateIP:  ip,
+				UpdateIP:  ip,
+				SectionID: row.ID,
+				Content:   p.Content,
+			}).Error != nil {
+				return errors.New("保存失败"), ""
+			}
+			return nil, ""
+		})
+		c.SuccessJson("success")
+	}
+}
 
 // 我的收入
-func apiAuthorIncomeAction(c *core.GContent) {}
+func apiAuthorIncomeAction(c *core.GContent) {
+	p := &core.ApiParam{}
+	if e := c.BindJson(p); e != nil {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	uid := c.GetUserID()
+	db := getDB(c)
+	ret := map[string]any{
+		"income": 0,
+		"list":   []any{},
+	}
+	buyList := []model.NovelBuyLog{}
+	if p.ID == 0 { //总收入
+		sum := uint(0)
+		ilist := []model.NovelInfo{}
+		db.Where("user_id=?", uid).Find(&ilist)
+		for _, item := range ilist {
+			sum += item.TotalIncome
+		}
+		ret["income"] = sum
+		db.Where("author_id=?", uid).Order("id DESC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&buyList)
+	} else { //单本收入
+		info := &model.NovelInfo{}
+		db.First(info, p.ID)
+		if info.ID == 0 || info.UserID != uid {
+			c.FailJson(403, "内容不存在")
+			return
+		}
+		ret["income"] = info.TotalIncome
+		db.Where("author_id=? AND novel_id=?", uid, p.ID).Order("id DESC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&buyList)
+	}
+	ret["list"] = buyList
+	c.SuccessJson(ret)
+}
 
 // 申请提现
-func apiAuthorApplyTxAction(c *core.GContent) {}
+func apiAuthorApplyTxAction(c *core.GContent) {
+	p := &model.NovelTxLog{}
+	if e := c.BindJson(p); e != nil || p.Amount == 0 {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	db := getDB(c)
+	uid := c.GetUserID()
+
+	p.UserID = uid
+	p.CreateIP = c.GetIP()
+	p.UpdateIP = c.GetIP()
+	p.Date = time.Now()
+
+	lastRow := &model.NovelTxLog{}
+
+	db.Where("user_id=?", uid).Order("id DESC").First(lastRow)
+	if lastRow.ID > 0 {
+		if p.Date.Sub(lastRow.Date) < 24*time.Hour {
+			c.FailJson(403, "24小时内只能提现一次")
+			return
+		}
+	}
+	if db.Save(p).Error == nil {
+		c.SuccessJson("success")
+		return
+	}
+	c.FailJson(403, "提现失败")
+}
 
 // 提现列表
-func apiAuthorTxListAction(c *core.GContent) {}
+func apiAuthorTxListAction(c *core.GContent) {
+	p := &core.ApiParam{}
+	if e := c.BindJson(p); e != nil {
+		c.FailJson(403, "参数错误")
+		return
+	}
+	list := []model.NovelTxLog{}
+	getDB(c).Where("user_id=?", c.GetUserID()).Order("id DESC").Offset(p.GetOffset()).Limit(p.GetPageSize()).Find(&list)
+	c.SuccessJson(list)
+}
